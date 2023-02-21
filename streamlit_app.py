104a4c3a2161087d8dc261362cec146f57442beb
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-02-21 14:49:23 +0300

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.set_page_config(
 # We create a set of columns to display the logo and the heading next to each other.
 
 
-c1, c2, c3 = st.columns([2, 4, 4])
+c1, c2, c3 = st.columns([8, 4, 4])
 
 # The snowflake logo will be displayed in the first column, on the left.
 with c1:
