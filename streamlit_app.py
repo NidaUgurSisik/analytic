4b28ed24b5700b04a1109f09dc9417259a79adf7
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-02-24 13:01:50 +0300

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True,layout="centered", page_title="Analytic", page_icon="❄️")
 
 
 
