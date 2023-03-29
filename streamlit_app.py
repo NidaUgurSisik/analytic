2ca447b7966ec6805d4b107a6ebead7877f465df
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-29 13:26:23 +0300

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,10 +72,9 @@ with c2:
 
     labels = shows['Industry'].value_counts().index
     sizes = shows['Industry'].value_counts()
-    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
 
     fig1, ax1 = plt.subplots()
-    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
             shadow=True, startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
