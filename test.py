28f0d44fe620d0746bd107d077deaefab0aa885a
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-29 12:38:10 +0300

    a

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from geopy.geocoders import Nominatim
 # Initialize Nominatim API
 geolocator = Nominatim(user_agent="MyApp")
 
-location = geolocator.geocode("Arnhem-Nijmegen Metropolitan Area")
+location = geolocator.geocode("Santiago de Quer  taro")
 
 print(location)
-#print("The latitude of the location is: ", location.latitude)
-#print("The longitude of the location is: ", location.longitude)
+print("The latitude of the location is: ", location.latitude)
+print("The longitude of the location is: ", location.longitude)
