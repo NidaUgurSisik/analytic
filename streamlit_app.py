26d121f179929f4e0c7d93bce834b247f6357288
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-29 12:57:05 +0300

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ import numpy as np
 import pydeck as pdk
 from geopy.exc import GeocoderTimedOut
 from geopy.geocoders import Nominatim
-    
+import matplotlib.pyplot as plt
+
 
 ############ 2. SETTING UP THE PAGE LAYOUT AND TITLE ############
 
@@ -69,6 +70,16 @@ with c2:
         countcountry= shows['Country/Region'].value_counts()
         st.bar_chart(countcountry[:15])
 
+    labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    sizes = [15, 30, 45, 10]
+    explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    st.pyplot(fig1)
         
 
         
