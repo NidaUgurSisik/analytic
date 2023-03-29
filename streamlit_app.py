0e7063b01f49a7420a6c2709d727bdb49efafd3c
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-29 13:27:26 +0300

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ with c2:
 
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-            shadow=True, startangle=90)
+             startangle=90)
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     st.pyplot(fig1)
